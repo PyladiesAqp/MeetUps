42a990ede3fdfb1fd6ce22d43f73ad2bf27a17f6
PyladiesAqp <aqp@pyladies.com>
2019-02-16 05:10:01 -0500

    Revert "updating"
    This reverts commit b5020712ce75f8ab78965a669f2030c47fa9ee1c.

--- MeetUp VI/pydata/db_create.py
+++
@@ -1,9 +0,0 @@
-import cx_Oracle
-
-con=cx_Oracle.connect("pydatabase","contra")
-
-cur=con.cursor()
-cur.execute('create table usuarios (nombre varchar(11) not null,apellido varchar(11) not null ,dni int not null primary key, edad int not null) ')
-
-cur.close()
-con.close()
